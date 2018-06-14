29dd7445573481e757be1d1763cc24c146e30521
Peter Marsh <p.marsh@mac.com>
2018-06-14 11:27:52 -0400

    added user_id to insert posts

--- queries/04_insert_posts.sql
+++ queries/04_insert_posts.sql
@@ -1,2 +1,2 @@
-INSERT INTO posts (title, body, 1) 
+INSERT INTO posts (title, body, user_id)
 VALUES ('Admin announcement', 'This API is awesome!', 1), ('Just a test', 'This is just a test. I repeat, this is just a test.', 1);
